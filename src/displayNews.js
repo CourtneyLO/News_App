ae44366447796a933f1bbc95ef4c55551a42f1cc
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-15 14:32:08 +0000

    Adds news headline app

--- src/displayNews.js
+++ src/displayNews.js
@@ -2,9 +2,9 @@ function DisplayNews() {
 
 }
 
-DisplayNews.prototype.newsHeadlines = function () {
-  var headline = document.getElementById("headline")
-  for (var i = 0; i < this.xhr.myText.response.results.length; i += 1) {
-    headline.innerHTML += "<li id='headline'>" + this.xhr.myText.response.results[i].webTitle + "</li>"
+  DisplayNews.prototype.newsHeadlines = function () {
+    var headline = document.getElementById("headline")
+      for (var i = 0; i < this.xhr.myText.response.results.length; i += 1) {
+      headline.innerHTML += '<li> <a id="headline" href="#articles' + i + '">' + this.xhr.myText.response.results[i].webTitle + '</a></li>'
   }
   }
