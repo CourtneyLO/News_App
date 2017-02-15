614061b2c32052276236a4879498abb236cafd3d
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-15 16:02:22 +0000

    Adds full article

--- src/NewsController.js
+++ src/NewsController.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     var newsApp = new NewsApp();
     var displayNews = new DisplayNews()
+    window.addEventListener("hashchange", displayNews.newsArticle.bind(newsApp))
     newsApp.getNews(displayNews.newsHeadlines.bind(newsApp))
 }
--- src/displayNews.js
+++ src/displayNews.js
@@ -4,8 +4,18 @@ function DisplayNews() {
 
   DisplayNews.prototype.newsHeadlines = function () {
     var headline = document.getElementById("headline")
-      for (var i = 0; i < this.xhr.myText.response.results.length; i += 1) {
+      for (var i = 0; i < this.xhr.myText.response.results.length; i ++) {
         var number = i + 1
-      headline.innerHTML += '<li> <a id="headline" href="#article' + number + '">' + this.xhr.myText.response.results[i].webTitle + '</a></li>'
+      headline.innerHTML += '<li> <a id="headline" href="#article/' + number + '">' + this.xhr.myText.response.results[i].webTitle + '</a></li>'
+      }
   }
+
+  DisplayNews.prototype.newsArticle = function () {
+    var newsArticleId = window.location.hash.split("/")[1];
+    var headline = document.getElementById("heading");
+    var date = document.getElementById("date");
+    var article = document.getElementById("article");
+    headline.innerHTML = this.xhr.myText.response.results[newsArticleId].webTitle
+    date.innerHTML = this.xhr.myText.response.results[newsArticleId].fields.lastModified
+    article.innerHTML = this.xhr.myText.response.results[newsArticleId].fields.bodyText
   }
