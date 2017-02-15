48758391483e70b4959ec25c4468c2585dd9714b
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-15 11:22:40 +0000

    Adds a NewsController and index.html file

--- src/NewsApp.js
+++ src/NewsApp.js
@@ -3,6 +3,7 @@ function NewsApp() {
 }
 
   NewsApp.prototype.getNews = function() {
+    console.log("Get News")
     this.xhr = new XMLHttpRequest()
     var url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=all&from-date=2017-02-15"
     this.xhr.open("GET", url , true);
@@ -13,6 +14,3 @@ function NewsApp() {
     }
     this.xhr.send()
   }
-
-newsApp = new NewsApp()
-newsApp.getNews()
---
+++ src/NewsController.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    console.log("viewController")
+    newsApp = new NewsApp()
+    newsApp.getNews()
+}
