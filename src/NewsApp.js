2d0ad468405d4bad3d7b739c890509d53ea8e8ed
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-15 11:01:43 +0000

    removes console.log

--- src/NewsApp.js
+++ src/NewsApp.js
@@ -6,7 +6,6 @@ function NewsApp() {
     this.xhr = new XMLHttpRequest()
     var url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=all&from-date=2017-02-15"
     this.xhr.open("GET", url , true);
-    console.log(this.xhr)
     this.xhr.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         this.myText = JSON.parse(this.responseText);
